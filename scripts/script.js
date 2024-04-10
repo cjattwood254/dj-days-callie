fd7150dc1a435a7d7755090b69d1ad9af41c7703
Callie <cjattwood254@gmail.com>
2024-04-09 19:04:50 -0500

    completed styling for now and added some js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
-class Album Display {
+class AlbumCollection {
     constructor(url, artist, title){
         this.url = url;
         this.artist = artist;
         this.title = title;
     }
 }
+
+// if AlbumDisplay.length <10, display "oops, you don't have enough albums to start this set!"
+// ideally calculate number of albums left to get to 10
+
+var n = 9 - AlbumCollection.length
+
+function checkSetLength(collectionArray){
+    if(collectionArray.length < 10){
+        console.log(`Oops! It looks like you need ${n} more albums to play your set.`)
+    }
+}
+
+document.getElementById('play-set').onclick = () => {
+    // display error message or play set
+};
