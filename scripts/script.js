7e28f873fd129904466cf6f4a0abc6d20fcb74dc
Callie <cjattwood254@gmail.com>
2024-04-12 16:03:46 -0500

    added content to script.js from demo, not yet working

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,49 @@
-class AlbumCollection {
-    constructor(url, artist, title){
-        this.url = url;
-        this.artist = artist;
-        this.title = title;
-    }
+function fetchAlbums(){
+    fetch("/api/albums")
+        .then(response => response.json())
+        .then(albums => displayAlbums(albums))
+        .catch(err => console.error("Error fetching albums: ", err));
 }
+
+function addAlbum(){
+    const art = document.getElementById("album-image").value;
+    const title = document.getElementById("album-title").value;
+    const artist = document.getElementById("album-artist").value;
+    const genre = document.getElementById("album-genre").value;
+    const newAlbum = {art, title, artist, genre};
+
+    fetch("/api/albums", {
+        method: "POST",
+        headers:{
+            "Content Type": "application.json",
+        },
+        body: JSON.stringify(newAlbum),
+    })
+    .then(response => {
+        if(response.ok){
+            return fetchAlbums();
+        }
+        throw new Error("Failed to add album.");
+    })
+    .catch(err => console.error("Error adding album: ", err));
+    document.getElementById("album-image").value = " ";
+    document.getElementById("album-title").value = " ";
+    document.getElementById("album-artist").value = " ";
+    document.getElementById("album-genre").value = " ";
+}
+
+function displayAlbums(albums){
+    const albumsList = document.getElementById("albumsList");
+    albumsList.innerHTML = " ";
+
+    albums.forEach(album =>{
+        const p = document.createElement("p");
+        p.textContent = `${album.album-image} ${album.album-title} ${album.album-artist} ${album.album-genre}`;
+        albumsList.appendChild(p);
+    });
+}
+
+fetchAlbums();
 
 // if AlbumDisplay.length <10, display "oops, you don't have enough albums to start this set!"
 // ideally calculate number of albums left to get to 10
